79d5836bd6d5bf27648eca94c301655d5f3602e6
GitGuru420 <raisulislam1892@gmail.com>
2025-03-28 00:21:40 +0600

    CSE Fundamental With Phitron 2025

--- 2 Introduction to C++ for DSA/Week-01 Introduction to C++/Module-01 Basic C++/Lecture/g_ternary_operator.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    // (condition) ? T : F
-    // int x = 6;
-    int x = 9;
-    (x%2 == 0) ? cout << "Even\n" : cout << "Odd\n";
-    return 0;
-}
